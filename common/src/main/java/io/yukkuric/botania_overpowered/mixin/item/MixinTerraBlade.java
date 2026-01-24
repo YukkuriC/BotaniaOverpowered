132e841ecf39161556f7cff8c586efd3504496d2
YukkuriC <799433638@qq.com>
2026-01-24 18:29:59 +0800

    manually supporting AlfsteelSword beam

--- common/src/main/java/io/yukkuric/botania_overpowered/mixin/item/MixinTerraBlade.java
+++ common/src/main/java/io/yukkuric/botania_overpowered/mixin/item/MixinTerraBlade.java
@@ -5,6 +5,7 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import io.yukkuric.botania_overpowered.BotaniaOPConfig;
 import io.yukkuric.botania_overpowered.helpers.WeaponHelpers;
+import mythicbotany.alftools.AlfsteelSword;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.ThrowableProjectile;
@@ -12,7 +13,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import vazkii.botania.common.item.equipment.tool.terrasteel.TerraBladeItem;
 
-@Mixin(TerraBladeItem.class)
+@Mixin(value = {
+        TerraBladeItem.class,
+        AlfsteelSword.class,
+})
 public class MixinTerraBlade {
     @WrapOperation(method = "updateBurst", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"))
     boolean burstWithBlade(LivingEntity instance, DamageSource source, float amount, Operation<Boolean> original, @Local ThrowableProjectile entity) {
